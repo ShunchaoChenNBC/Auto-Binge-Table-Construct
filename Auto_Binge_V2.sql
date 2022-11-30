dcca0add5567a5b838476447d2958b59a2f50ced
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2022-11-30 11:25:37 -0500

    Add files via upload
    The code to construct Auto-Binge table

---
+++ Auto_Binge_V2.sql
@@ -0,0 +1,53 @@
+with Raw_Clicks as (SELECT
+post_evar56 as Adobe_Tracking_ID, 
+DATE(timestamp(post_cust_hit_time_gmt), "America/New_York") AS Adobe_Date,
+DATETIME(timestamp(post_cust_hit_time_gmt), "America/New_York") AS Adobe_Timestamp,
+post_evar19 as Player_Event,
+post_evar7 as Binge_Details
+FROM `nbcu-ds-prod-001.feed.adobe_clickstream` 
+WHERE post_cust_hit_time_gmt is not null
+and post_evar19 is not null
+and DATE(timestamp(post_cust_hit_time_gmt), "America/New_York") <= DATE_ADD(CURRENT_DATE(), INTERVAL -1 day))
+SELECT 
+Adobe_Tracking_ID,
+Adobe_Date,
+Adobe_Timestamp,
+Player_Event,
+Binge_Details,
+case when Binge_Details like "%Programme-cue-up%auto-play" then "Auto-Play" 
+     when Binge_Details like "%Programme-cue-up%click" then "Clicked-Up-Next" 
+     when Binge_Details like "%Programme-cue-up%dismiss" then "Dismiss" 
+     when Player_Event like "%details:%" and Binge_Details is not null then "Manual-Selection"
+else 'Unattributed' end as Video_Start_Type,
+'' device_name,
+'' Feeder_Video,
+'' Feeder_Video_Id,
+case when Player_Event like "%details:%" and Binge_Details is not null then REGEXP_REPLACE(Player_Event, r'\w+:', '')
+     when Binge_Details like "%Programme-cue-up%auto-play" then  REGEXP_EXTRACT(Binge_Details, r"[|][|](.*)[|]")
+     when Binge_Details like "%Programme-cue-up%click" then  REGEXP_EXTRACT(Binge_Details, r"[|][|](.*)[|]")
+else null end as Display_Name,
+'' video_id,
+'' link_tracking,
+'' deeplink_flag,
+null num_seconds_played_no_ads
+FROM Raw_Clicks
+union all
+SELECT
+adobe_tracking_id as Adobe_Tracking_ID,
+adobe_date as Adobe_Date,
+TIMESTAMP_ADD(adobe_timestamp , INTERVAL -40 second) as Adobe_Timestamp,--reduce the discrepancy between two tables
+'' Player_Event,
+'' Binge_Details,
+'Vdo_End' as Video_Start_Type,
+device_name,
+Lag(display_name) over (partition by adobe_tracking_id,adobe_date order by adobe_timestamp) as Feeder_Video,
+Lag(video_id) over (partition by adobe_tracking_id,adobe_date order by adobe_timestamp) as Feeder_Video_Id,
+display_name as Display_Name,
+video_id,
+link_tracking,
+deeplink_flag,
+num_seconds_played_no_ads
+FROM 
+`nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO`
+where DATE(timestamp(adobe_date), "America/New_York") <= DATE_ADD(CURRENT_DATE(), INTERVAL -1 day)
+order by 1, 2, 3;
