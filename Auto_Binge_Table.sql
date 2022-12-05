639a66e68bef2ef05ce72bafe0667fede0891dcb
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2022-12-04 20:44:54 -0500

    Update Auto_Binge_Table.sql
    Update the regular expression to extend the code to cover deeplink and railclick.

--- Auto_Binge_Table.sql
+++ Auto_Binge_Table.sql
@@ -5,37 +5,43 @@ DATETIME(timestamp(post_cust_hit_time_gmt), "America/New_York") AS Adobe_Timesta
 post_evar19 as Player_Event,
 post_evar7 as Binge_Details
 FROM `nbcu-ds-prod-001.feed.adobe_clickstream` 
-WHERE post_cust_hit_time_gmt is not null
-and post_evar19 is not null
-and DATE(timestamp(post_cust_hit_time_gmt), "America/New_York") <= DATE_ADD(CURRENT_DATE(), INTERVAL -1 day))
+WHERE post_evar56 is not null
+and post_cust_hit_time_gmt is not null 
+and post_evar7 is not null
+and post_evar7 not like "%display"
+and extract(YEAR from timestamp(post_cust_hit_time_gmt))=2022 
+and extract(month from timestamp(post_cust_hit_time_gmt))=11
+and extract(day from timestamp(post_cust_hit_time_gmt))=26) -- select timerange:DATE(timestamp(post_cust_hit_time_gmt), "America/New_York") <= DATE_ADD(CURRENT_DATE(), INTERVAL -1 day))
+
 SELECT 
 Adobe_Tracking_ID,
 Adobe_Date,
 Adobe_Timestamp,
 Player_Event,
 Binge_Details,
-case when Binge_Details like "%Programme-cue-up%auto-play" then "Auto-Play" 
-     when Binge_Details like "%Programme-cue-up%click" then "Clicked-Up-Next" 
-     when Binge_Details like "%Programme-cue-up%dismiss" then "Dismiss" 
+case when Binge_Details like "%auto-play" then "Auto-Play" 
+     when Binge_Details like '%cue%up%click' then "Clicked-Up-Next" 
+     when Binge_Details like "%dismiss" then "Dismiss" 
      when Player_Event like "%details:%" and Binge_Details is not null then "Manual-Selection"
-else 'Unattributed' end as Video_Start_Type,
+     when Binge_Details like '%deeplink%' then "Manual-Selection_(DeepLink)"
+     when Binge_Details like 'rail%click'then "Manual-Selection_(RailClick)" 
+else null end as Video_Start_Type,
 '' device_name,
 '' Feeder_Video,
 '' Feeder_Video_Id,
 case when Player_Event like "%details:%" and Binge_Details is not null then REGEXP_REPLACE(Player_Event, r'\w+:', '')
-     when Binge_Details like "%Programme-cue-up%auto-play" then  REGEXP_EXTRACT(Binge_Details, r"[|][|](.*)[|]")
-     when Binge_Details like "%Programme-cue-up%click" then  REGEXP_EXTRACT(Binge_Details, r"[|][|](.*)[|]")
+     when Binge_Details like "%auto-play" then  REGEXP_EXTRACT(Binge_Details, r"[|][|](.*)[|]")
+     when Binge_Details like '%cue%up%click' then  REGEXP_EXTRACT(Binge_Details, r"[|][|](.*)[|]")
+     when Binge_Details like 'rail%click'then REGEXP_EXTRACT(Binge_Details, r"[|]([a-zA-Z0-9\s-.:]+)[|]click")
 else null end as Display_Name,
 '' video_id,
-'' link_tracking,
-'' deeplink_flag,
 null num_seconds_played_no_ads
 FROM Raw_Clicks
 union all
 SELECT
 adobe_tracking_id as Adobe_Tracking_ID,
 adobe_date as Adobe_Date,
-TIMESTAMP_ADD(adobe_timestamp , INTERVAL -40 second) as Adobe_Timestamp,--reduce the discrepancy between two tables
+TIMESTAMP_ADD(adobe_timestamp , INTERVAL -40 second) as Adobe_Timestamp,
 '' Player_Event,
 '' Binge_Details,
 'Vdo_End' as Video_Start_Type,
@@ -44,10 +50,10 @@ Lag(display_name) over (partition by adobe_tracking_id,adobe_date order by adobe
 Lag(video_id) over (partition by adobe_tracking_id,adobe_date order by adobe_timestamp) as Feeder_Video_Id,
 display_name as Display_Name,
 video_id,
-link_tracking,
-deeplink_flag,
 num_seconds_played_no_ads
 FROM 
 `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO`
-where DATE(timestamp(adobe_date), "America/New_York") <= DATE_ADD(CURRENT_DATE(), INTERVAL -1 day)
-order by 1, 2, 3;
+where adobe_tracking_ID is not null
+and adobe_date = '2022-11-26'
+and media_load = False and num_seconds_played_with_ads > 0
+order by Adobe_Tracking_ID, Adobe_Timestamp;
