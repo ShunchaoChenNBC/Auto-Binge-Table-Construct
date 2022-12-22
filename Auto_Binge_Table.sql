e9ac60d9ba0b66f3aceca8fe6d90e0916f5cdfe4
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2022-12-22 13:57:06 -0500

    Null and Manual Selection Issue Fixed
    1. Convert all "" to null to make null consistent
    2. Convert rail click and deep link manual selections to "manual selection" label

--- Auto_Binge_Table.sql
+++ Auto_Binge_Table.sql
@@ -23,18 +23,18 @@ case when Binge_Details like "%auto-play" then "Auto-Play"
      when Binge_Details like '%cue%up%click' then "Clicked-Up-Next" 
      when Binge_Details like "%dismiss" then "Dismiss" 
      when Player_Event like "%details:%" and Binge_Details is not null then "Manual-Selection"
-     when Binge_Details like '%deeplink%' then "Manual-Selection_(DeepLink)"
-     when Binge_Details like 'rail%click'then "Manual-Selection_(RailClick)" 
+     when Binge_Details like '%deeplink%' then "Manual-Selection"
+     when Binge_Details like 'rail%click'then "Manual-Selection" 
 else null end as Video_Start_Type,
-'' device_name,
-'' Feeder_Video,
-'' Feeder_Video_Id,
+null device_name,
+null Feeder_Video,
+null Feeder_Video_Id,
 case when Player_Event like "%details:%" and Binge_Details is not null then REGEXP_REPLACE(Player_Event, r'\w+:', '')
      when Binge_Details like "%auto-play" then  REGEXP_EXTRACT(Binge_Details, r"[|][|](.*)[|]")
      when Binge_Details like '%cue%up%click' then  REGEXP_EXTRACT(Binge_Details, r"[|][|](.*)[|]")
      when Binge_Details like 'rail%click'then REGEXP_EXTRACT(Binge_Details, r"[|]([a-zA-Z0-9\s-.:]+)[|]click")
 else null end as Display_Name,
-'' video_id,
+null video_id,
 null num_seconds_played_no_ads
 FROM Raw_Clicks
 union all
@@ -42,8 +42,8 @@ SELECT
 adobe_tracking_id as Adobe_Tracking_ID,
 adobe_date as Adobe_Date,
 TIMESTAMP_ADD(adobe_timestamp , INTERVAL -40 second) as Adobe_Timestamp,
-'' Player_Event,
-'' Binge_Details,
+null Player_Event,
+null Binge_Details,
 'Vdo_End' as Video_Start_Type,
 device_name,
 Lag(display_name) over (partition by adobe_tracking_id,adobe_date order by adobe_timestamp) as Feeder_Video,
